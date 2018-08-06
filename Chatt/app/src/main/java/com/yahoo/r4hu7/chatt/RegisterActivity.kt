bdd0c0642fb01e48521e0a127c2c3de2d564a188
Rahul Gaur <rench009@gmail.com>
2018-08-06 13:40:41 +0530

    Chatt added

---
+++ Chatt/app/src/main/java/com/yahoo/r4hu7/chatt/RegisterActivity.kt
@@ -0,0 +1,15 @@
+package com.yahoo.r4hu7.chatt
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+    }
+}
